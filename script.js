519ed1dd49e6d418a50ece3f91cd70e95714c6c3
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-02-20 14:24:39 +0600

    comment removed

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// function getPin() {
-//     document.getElementById('pinInput').value = Math.round(Math.random() * 10000);
-// }
 function getPin() {
-    // const random = Math.random() * 10000;
-    // const pin = (random + '').split('.')[0];
     const random = Math.round(Math.random() * 10000);
     const pin = (random + '');
     if (pin.length === 4) {
         return pin;
     }
     else {
-        // console.log('shorter pin', pin);
         return getPin();
     }
 }
@@ -25,9 +19,6 @@ const buttonContainer = document.getElementById('digit-container');
 buttonContainer.addEventListener('click', function (event) {
     const digit = event.target.innerText;
     if (isNaN(digit)) {
-        //handle backspace
-        //handle clear
-        // console.log('handle non digit');
         if (digit === 'C') {
             const typedInput = document.getElementById('typed-pin');
             typedInput.value = '';
@@ -48,19 +39,11 @@ function verifyPin() {
         const pin = document.getElementById('pin').value;
         const typedPin = document.getElementById('typed-pin').value;
         if (pin === typedPin) {
-            /*const correct = document.getElementById('correct-pin');
-            correct.style.display = "block";
-            const incorrect = document.getElementById('incorrect-pin');
-            incorrect.style.display = "none";*/
             displayMatchResult("block", "none");
             document.getElementById('pin').value = "";
             document.getElementById('typed-pin').value = '';
         }
         else {
-            /*const correct = document.getElementById('correct-pin');
-            correct.style.display = "none";
-            const incorrect = document.getElementById('incorrect-pin');
-            incorrect.style.display = "block";*/
             displayMatchResult("none", "block");
             let count = Number(document.getElementById('try-count').textContent) - 1;
             if (count >= 1) {
